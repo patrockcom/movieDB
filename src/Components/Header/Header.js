32ed0c692a7cc4cb6df6a448789f57315ace8b74
patrick.roth <patrick.roth@me.com>
2021-02-16 12:09:08 +0100

    first commit

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,14 @@
+// HEADER.JS
+
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div className="Header">
+        <h1>Header Components</h1>
+        </div>
+    )
+}
+
+export default Header;
